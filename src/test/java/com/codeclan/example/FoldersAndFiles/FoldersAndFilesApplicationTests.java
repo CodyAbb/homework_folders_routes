841f09a903fc285b3db24ecf0361a79e384f676c
Cody Abbott <cody.abbott16@gmail.com>
2020-02-25 15:56:03 +0000

    Initial file setup

---
+++ src/test/java/com/codeclan/example/FoldersAndFiles/FoldersAndFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.FoldersAndFiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoldersAndFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
